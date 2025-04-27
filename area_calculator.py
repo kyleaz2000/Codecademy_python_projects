25fa5388a6fb1d7709866f211d68a38b8c527fc4
kyleaz2000 <114624123+kyleaz2000@users.noreply.github.com>
2025-04-27 14:17:48 -0700

    Create area_calculator.py

---
+++ area_calculator.py
@@ -0,0 +1,21 @@
+# Area Calculator
+#Another error finding activity where there was a missing comma, an incorrect operator, and incorrect name. All have been corrected below.
+
+import math
+
+base = 20
+height = 30
+area = base * height / 2
+
+print("The triangle area is", area)
+
+length = 2
+width = 12
+area = length * width
+
+print("The rectangle area is", area )
+    
+radius = 36
+area = math.pi * radius * radius
+    
+print("The circle area is", area)
